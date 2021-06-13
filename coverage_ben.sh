9c77bd110f1ca34046c7bb388bd6be2cc5b502fb
Gourab <19bcs118@iiitdwd.ac.in>
2021-06-13 10:43:21 +0530

    Tweaked Coverage script

--- coverage_ben.sh
+++ coverage_ben.sh
@@ -1,2 +1,8 @@
 cat ../apertium-ben/Texts/sample_ben.txt | apertium -d . ben-morph | sed 's/\$\W*\^/$\n^/g' > /tmp/ben-coverage.txt
-calc `cat /tmp/ben-coverage.txt | grep -v '\*' | wc -l `/`cat /tmp/ben-coverage.txt | wc -l`
+ 
+TOTAL=`cat /tmp/ben-coverage.txt | wc -l`
+KNOWN=`cat /tmp/ben-coverage.txt | grep -v '\*' | wc -l `
+
+COVER=`calc $KNOWN / $TOTAL`;
+
+echo "ben: $COVER"
